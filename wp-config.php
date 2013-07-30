fbba6950629f0440b1515348e5a1eef9fdf1cf8f
sandersgraphics <david@sandersgraphics.co.uk>
2013-07-30 20:39:22 +0100

    config update

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+
+define('DB_NAME', 'sandersgraphics');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+
+define('DB_USER', 'sandersgraphics');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+
+define('DB_PASSWORD', 'Kurzweil@1970');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '37.148.204.42');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
