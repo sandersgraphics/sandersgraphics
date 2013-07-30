513e45469e30e1b87e23ea75d7db655be8ca1cd9
sandersgraphics <david@sandersgraphics.co.uk>
2013-07-30 20:43:30 +0100

    adding sanders graphics theme folder

--- wp-config.php
+++ wp-config.php
@@ -17,18 +17,18 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 
-define('DB_NAME', 'sandersgraphics');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
 
-define('DB_USER', 'sandersgraphics');
+define('DB_USER', 'root');
 
 /** MySQL database password */
 
-define('DB_PASSWORD', 'Kurzweil@1970');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', '37.148.204.42');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
