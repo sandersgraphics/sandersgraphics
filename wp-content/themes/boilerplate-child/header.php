5dafe40bf08f3348d226ca92ea5afa7ca2ebe813
sandersgraphics <david@sandersgraphics.co.uk>
2013-10-03 15:57:38 +0100

    latest update 3

--- wp-content/themes/boilerplate-child/header.php
+++ wp-content/themes/boilerplate-child/header.php
@@ -16,6 +16,7 @@
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php
 			/*
 			 * Print the <title> tag based on what is being viewed.
